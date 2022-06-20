718f05a4f4cfee509b297f757060c206a3d56a8b
danilzlu <37219798+danilzlu@users.noreply.github.com>
2022-06-20 20:20:01 +0300

    Refactored

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def mainTest():
                 abstract = Abstracts()
                 abstract.getInfoFromFile(infile)
                 abstracts_list.append(abstract)
-    makeBookOfAbstracts(abstracts_list)
+    print("\n".join(abstracts_list))
 
 
 def notMainTest(name=''):
